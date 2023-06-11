a9b5a3744b61fd9aac2f4a198b793022d093fe78
jaraldj <jarald.jsj@gmail.com>
2023-06-11 17:31:03 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'backimg':
+          'url("https://images.unsplash.com/photo-1518527989017-5baca7a58d3c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80")',
+      },
+    },
+  },
+  plugins: [],
+};
