e0ac4bae9f2548f401027503389ee33a3e101bc6
jaraldj <jarald.jsj@gmail.com>
2023-12-02 19:32:04 +0530

    favicon added

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,8 @@ module.exports = {
           'url("https://images.unsplash.com/photo-1518527989017-5baca7a58d3c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80")',
         'backimg2':
           'url("https://cdn.pixabay.com/photo/2021/09/16/21/27/container-ship-6631117_1280.jpg")',
+        'backimg3' :
+          'url("https://drive.google.com/uc?export=view&id=1m2oWLMFfu93HlVyPzG406U6gMNwZzUFc")',
       },
     },
   },
