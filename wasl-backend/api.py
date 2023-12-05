29bc6e0023b5b53552cd3bc4084bbb506313c68a
akshayCODE27 <100348993+akshayCODE27@users.noreply.github.com>
2023-12-05 21:20:05 +0530

    ssl certificate loaction change

--- wasl-backend/api.py
+++ wasl-backend/api.py
@@ -173,4 +173,4 @@ if __name__ == "__main__":
 
     PRODUCTS = get_products()
 
-    uvicorn.run(app, host="0.0.0.0", port=7575, ssl_keyfile="/etc/letsencrypt/live/www.waslinternational.com/privkey.pem", ssl_certfile="/etc/letsencrypt/live/www.waslinternational.com/fullchain.pem")
+    uvicorn.run(app, host="0.0.0.0", port=7575, ssl_keyfile="ssl/privkey.pem", ssl_certfile="ssl/fullchain.pem")
