1fefb764e1a13b790b7416140b211daeecca53f2
jaraldj <jarald.jsj@gmail.com>
2023-12-18 00:32:32 +0530

    temp product changes

--- src/js/script.js
+++ src/js/script.js
@@ -1,119 +1,180 @@
-let productList;
+import {productList} from "./product.js";
+
+// let productList;
 
 //product api
-const apiUrl = "http://34.202.161.249:7575/wasl/products";
+// const apiUrl = "https://34.202.161.249:7575/wasl/products";
 
 // contact api
-const contactApi = "http://34.202.161.249:7575/wasl/contact_us";
+// const contactApi = "https://34.202.161.249:7575/wasl/contact_us";
 
-window.addEventListener("scroll", function () {
-  const navbar = document.getElementById("navbar");
-  const navLinks = document.querySelector(".nav-links");
-  if (window.scrollY > 50) {
-    navbar.classList.remove("bg-transparent");
-    navbar.classList.add("bg-blue-950");
-  } else {
-    navbar.classList.remove("bg-blue-950");
-    navbar.classList.add("bg-transparent");
-    navLinks.classList.add("bg-transparent");
-  }
-});
+let productDisplay =
+      productList &&
+      productList.map((val, id) => {
+        return `<div class="shadow-lg shadow-blue-10 flex flex-col group p-4" key=${id}>
+          <img src="${val.productImg}" alt="product" />
+          <div class="pt-4 pb-8">
+            <h2 class="text-2xl text-blue-950 font-semibold">${val.productName}</h2>
+            <p class="text-blue-900">
+              ${val.productDesc}
+            </p>
+          </div>
+          <div class="mt-auto">
+            <a href="src/product.html?name=${val.productName}&id=${val.id}" class="float-right text-blue-600">Detail</a>
+          </div>
+        </div>`;
+      });
 
-const onToggleMenu = (icon) => {
-  const navbar = document.getElementById("navbar");
-  const menuIcon = icon;
-  const navLinks = document.querySelector(".nav-links");
-  navLinks.classList.toggle("hidden");
+    document.getElementById("prodLi").innerHTML = productDisplay
+      ? productDisplay.join("")
+      : "";
 
-  if (menuIcon.classList.contains("fa-bars")) {
-    menuIcon.classList.remove("fa-bars");
-    menuIcon.classList.add("fa-xmark");
-  } else {
-    menuIcon.classList.remove("fa-xmark");
-    menuIcon.classList.add("fa-bars");
-    // navLinks.classList.add("hidden");
-  }
-  if (menuIcon.classList.contains("fa-xmark")) {
-    menuIcon.classList.remove("bg-transparent");
-    menuIcon.classList.add("bg-blue-950");
-    navLinks.classList.remove("bg-transparent");
-    navLinks.classList.add("bg-blue-950");
-  } else {
-    navLinks.classList.remove("bg-blue-950");
-    navLinks.classList.add("bg-transparent");
-    menuIcon.classList.remove("bg-blue-950");
-    menuIcon.classList.add("bg-transparent");
-  }
+// window.addEventListener("scroll", function () {
+//   const navbar = document.getElementById("navbar");
+//   const navLinks = document.querySelector(".nav-links");
+//   if (window.scrollY > 50) {
+//     navbar.classList.remove("bg-transparent");
+//     navbar.classList.add("bg-blue-950");
+//   } else {
+//     navbar.classList.remove("bg-blue-950");
+//     navbar.classList.add("bg-transparent");
+//     navLinks.classList.add("bg-transparent");
+//   }
+// });
 
-  navLinks.addEventListener("click", function () {
-    menuIcon.classList.remove("fa-xmark");
-    menuIcon.classList.add("fa-bars");
-    navLinks.classList.add("hidden");
-  });
-};
+// const onToggleMenu = (icon) => {
+//   const navbar = document.getElementById("navbar");
+//   const menuIcon = icon;
+//   const navLinks = document.querySelector(".nav-links");
+//   navLinks.classList.toggle("hidden");
 
-const postData = async () => {
-  try {
-    const name = document.getElementById("nameInput").value;
-    const phone = document.getElementById("phoneInput").value;
-    const email = document.getElementById("emailInput").value;
-    const message = document.getElementById("messageInput").value;
+//   if (menuIcon.classList.contains("fa-bars")) {
+//     menuIcon.classList.remove("fa-bars");
+//     menuIcon.classList.add("fa-xmark");
+//   } else {
+//     menuIcon.classList.remove("fa-xmark");
+//     menuIcon.classList.add("fa-bars");
+//     // navLinks.classList.add("hidden");
+//   }
+//   if (menuIcon.classList.contains("fa-xmark")) {
+//     menuIcon.classList.remove("bg-transparent");
+//     menuIcon.classList.add("bg-blue-950");
+//     navLinks.classList.remove("bg-transparent");
+//     navLinks.classList.add("bg-blue-950");
+//   } else {
+//     navLinks.classList.remove("bg-blue-950");
+//     navLinks.classList.add("bg-transparent");
+//     menuIcon.classList.remove("bg-blue-950");
+//     menuIcon.classList.add("bg-transparent");
+//   }
 
-    const contactData = {
-      name: name,
-      phone: phone,
-      email: email,
-      message: message,
-    };
+//   navLinks.addEventListener("click", function () {
+//     menuIcon.classList.remove("fa-xmark");
+//     menuIcon.classList.add("fa-bars");
+//     navLinks.classList.add("hidden");
+//   });
+// };
 
-    console.log(contactData);
+// const postData = async () => {
+//   try {
+//     const name = document.getElementById("nameInput").value;
+//     const phone = document.getElementById("phoneInput").value;
+//     const email = document.getElementById("emailInput").value;
+//     const message = document.getElementById("messageInput").value;
 
-    const response = await fetch(contactApi, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        // Add any additional headers if needed
-      },
-      body: JSON.stringify(contactData),
-    });
+//     const contactData = {
+//       name: name,
+//       phone: phone,
+//       email: email,
+//       message: message,
+//     };
 
-    const data = await response.json();
-    // Handle the response from the API
-    console.log("API Response:", data);
-    if (data.status == true) {
-      document.getElementById("nameInput").value = ""
-      document.getElementById("phoneInput").value = ""
-      document.getElementById("emailInput").value = ""
-      document.getElementById("messageInput").value = ""
-    }
-  } catch (error) {
-    console.error("Error during API call:", error);
-  }
-};
+//     console.log(contactData);
 
-async function fetchData() {
-  const response = await fetch(apiUrl);
-  const data = await response.json();
-  productList = data;
-  let productDisplay =
-    productList &&
-    productList.map((val, id) => {
-      return `<div class="shadow-lg shadow-blue-10 flex flex-col group p-4">
-      <img src="${val.productImg}" alt="product" />
-      <div class="pt-4 pb-8">
-        <h2 class="text-2xl text-blue-950 font-semibold">${val.productName}</h2>
-        <p class="text-blue-900">
-          ${val.productDesc}
-        </p>
-      </div>
-      <div class="mt-auto">
-      <a href="src/product.html?name=${val.productName}&id=${val.id}" class="float-right text-blue-600">Detail</a>
-      </div>
-      </div>`;
-    });
-  document.getElementById("prodLi").innerHTML = productDisplay
-    ? productDisplay.join("")
-    : "";
-}
+//     const response = await fetch(contactApi, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//         // Add any additional headers if needed
+//       },
+//       body: JSON.stringify(contactData),
+//     });
 
-fetchData();
+//     const data = await response.json();
+//     // Handle the response from the API
+//     console.log("API Response:", data);
+//     if (data.status == true) {
+//       document.getElementById("nameInput").value = ""
+//       document.getElementById("phoneInput").value = ""
+//       document.getElementById("emailInput").value = ""
+//       document.getElementById("messageInput").value = ""
+//     }
+//   } catch (error) {
+//     console.error("Error during API call:", error);
+//   }
+// };
+
+// async function fetchData() {
+//   try {
+//     const response = await fetch(apiUrl);
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! Status: ${response.status}`);
+//     }
+
+//     const data = await response.json();
+//     const productList = data;
+
+//     let productDisplay =
+//       productList &&
+//       productList.map((val, id) => {
+//         return `<div class="shadow-lg shadow-blue-10 flex flex-col group p-4" key=${id}>
+//           <img src="${val.productImg}" alt="product" />
+//           <div class="pt-4 pb-8">
+//             <h2 class="text-2xl text-blue-950 font-semibold">${val.productName}</h2>
+//             <p class="text-blue-900">
+//               ${val.productDesc}
+//             </p>
+//           </div>
+//           <div class="mt-auto">
+//             <a href="src/product.html?name=${val.productName}&id=${val.id}" class="float-right text-blue-600">Detail</a>
+//           </div>
+//         </div>`;
+//       });
+
+//     document.getElementById("prodLi").innerHTML = productDisplay
+//       ? productDisplay.join("")
+//       : "";
+//   } catch (error) {
+//     console.error("Error fetching data:", error);
+//     // Handle the error, show an error message, etc.
+//   }
+// }
+
+// fetchData();
+
+// async function fetchData() {
+//   const response = await fetch(apiUrl);
+//   const data = await response.json();
+//   productList = data;
+//   let productDisplay =
+//     productList &&
+//     productList.map((val, id) => {
+//       return `<div class="shadow-lg shadow-blue-10 flex flex-col group p-4">
+//       <img src="${val.productImg}" alt="product" />
+//       <div class="pt-4 pb-8">
+//         <h2 class="text-2xl text-blue-950 font-semibold">${val.productName}</h2>
+//         <p class="text-blue-900">
+//           ${val.productDesc}
+//         </p>
+//       </div>
+//       <div class="mt-auto">
+//       <a href="src/product.html?name=${val.productName}&id=${val.id}" class="float-right text-blue-600">Detail</a>
+//       </div>
+//       </div>`;
+//     });
+//   document.getElementById("prodLi").innerHTML = productDisplay
+//     ? productDisplay.join("")
+//     : "";
+// }
+
+// fetchData();
--- src/js/singleProduct.js
+++ src/js/singleProduct.js
@@ -1,4 +1,5 @@
-let productList;
+import { productList } from "./product.js";
+// let productList;
 
 const apiUrl = 'http://34.202.161.249:7575/wasl/products';
 
@@ -7,39 +8,56 @@ const urlParams = new URLSearchParams(queryString);
 const nameurl = urlParams.get("name");
 const urlId = urlParams.get("id");
 
-async function fetchData() {
-  try {
-    const response = await fetch(apiUrl);
+let singleProdimg = productList
+.filter((val) => urlId == val.id)
+.map((val) => `<img src="${val.productImg}" alt="singleProduct" />`)
+.join("");
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
+let singleList = productList
+.filter((val) => urlId == val.id)
+.map((val) =>
+  val.singleProduct
+    .map((value) => `<li class="pb-4">${value}</li>`)
+    .join("")
+)
+.join("");
 
-    const data = await response.json();
-    productList = data;
+document.getElementById("singleProductImage").innerHTML = singleProdimg;
+document.getElementById("cocoLi").innerHTML = singleList;
 
-    let singleProdimg = productList
-      .filter((val) => urlId == val.id)
-      .map((val) => `<img src="${val.productImg}" alt="singleProduct" />`)
-      .join("");
+// async function fetchData() {
+//   try {
+//     const response = await fetch(apiUrl);
 
-    let singleList = productList
-      .filter((val) => urlId == val.id)
-      .map((val) =>
-        val.singleProduct
-          .map((value) => `<li class="pb-4">${value}</li>`)
-          .join("")
-      )
-      .join("");
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! Status: ${response.status}`);
+//     }
 
-    document.getElementById("singleProductImage").innerHTML = singleProdimg;
-    document.getElementById("cocoLi").innerHTML = singleList;
-  } catch (error) {
-    console.error('Error fetching data:', error);
-  }
-}
+//     const data = await response.json();
+//     productList = data;
 
-fetchData();
+//     let singleProdimg = productList
+//       .filter((val) => urlId == val.id)
+//       .map((val) => `<img src="${val.productImg}" alt="singleProduct" />`)
+//       .join("");
+
+//     let singleList = productList
+//       .filter((val) => urlId == val.id)
+//       .map((val) =>
+//         val.singleProduct
+//           .map((value) => `<li class="pb-4">${value}</li>`)
+//           .join("")
+//       )
+//       .join("");
+
+//     document.getElementById("singleProductImage").innerHTML = singleProdimg;
+//     document.getElementById("cocoLi").innerHTML = singleList;
+//   } catch (error) {
+//     console.error('Error fetching data:', error);
+//   }
+// }
+
+// fetchData();
 
 let whatsapp = `<a href="https://api.whatsapp.com/send?phone=+919659434344&text=Can%20I%20know%20detail%20about%20${nameurl}" class="flex items-start justify-center text-xl" target="_blank">
 <i class="fa-brands fa-whatsapp text-3xl lg:text-4xl"></i>
